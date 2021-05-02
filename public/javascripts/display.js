07312dd4daafdd0b01ad10cbbebf2f2f9dcd0e27
shackbu <eamonnstafford9@gmail.com>
2021-05-02 13:00:19 +0100

    gfytu
    uigyu

---
+++ public/javascripts/display.js
@@ -0,0 +1,23 @@
+function display()
+{
+    
+    let i =document.getElementById("myHiddenField").value;
+    if (i == undefined){i=0;}
+    
+    
+    
+    
+    
+    roomsString =document.getElementById("array").value;
+    var roomsArray =JSON.parse(roomsString);
+    document.getElementById("viewing").innerHTML=roomsArray[i].uid;
+    document.getElementById("cost").innerHTML=roomsArray[i].price;
+    document.getElementById("location").innerHTML=roomsArray[i].city;
+    document.getElementById("test").innerHTML=roomsArray[i].name;
+    
+
+
+    i++;
+    document.getElementById("myHiddenField").value = i;
+    
+}
