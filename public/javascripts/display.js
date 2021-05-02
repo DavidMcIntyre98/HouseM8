1d423171b112ebb365de3343e54506162557b98c
shackbu <eamonnstafford9@gmail.com>
2021-05-02 22:04:09 +0100

    useen
    Useen now working so users wont be shown same profile again

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,6 +1,38 @@
 function display()
-{
+{ 
+    console.log("pow");
+    //add the profile to the seen collection
+
     
+        var xhr = new XMLHttpRequest();
+        console.log("howya");
+        
+        xhr.open('POST', 'https://us-central1-housem8-8b9bf.cloudfunctions.net/useen');
+        xhr.setRequestHeader("Content-type", "application/json");
+        // Track the state changes of the request.
+        console.log("trying useen");
+        xhr.onreadystatechange = function () {
+            console.log("trying useen2");
+            var DONE = 4; // readyState 4 means the request is done.
+            var OK = 200; // status 200 is a successful return.
+            if (xhr.readyState === DONE) {
+                if (xhr.status === OK) {
+                    console.log("All good"); 
+                } else {
+                    console.log('Error: ' + xhr.status); // An error occurred during the request.
+                }
+            }
+        };
+    
+        xhr.send(JSON.stringify(
+            {"tid": getCookie('uid'), "lid": 	document.getElementById('viewing').value}
+        ));
+    
+
+
+
+
+
     let i =document.getElementById("myHiddenField").value;
     if (i == undefined){i=0;}
     
