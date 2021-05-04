38dfd8021a167cf8828a7fe5c56d6f213a166fcd
GooseInTheHoose <57994623+GooseInTheHoose@users.noreply.github.com>
2021-05-04 12:57:21 +0100

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -82,6 +82,20 @@ exports.postcomments = functions.https.onRequest((request, response) => {
     });
 });
 
+exports.image_data = functions.https.onRequest((request, response) => {
+    // 1. Receive comment data in here from user POST request
+    // 2. Connect to our Firestore database
+    cors(request, response, () => {
+
+        const currentTime = admin.firestore.Timestamp.now();
+        request.body.timestamp = currentTime;
+		
+        return admin.firestore().collection('imageData').add(request.body).then((snapshot) => {
+            response.send("Saved in the database");
+        });
+    });
+});
+
 exports.getcomments = functions.https.onRequest((request, response) => {
 
     cors(request, response, () => {
@@ -160,7 +174,6 @@ exports.createLprofile = functions.https.onRequest((request, response) => {
 exports.getrooms = functions.https.onRequest((request, response) => {
 	console.log('here');
 	cors(request, response, () => {
-		console.log("this should be the uid of the user:"+request.body);
 		var recieved = request.body;
 		let myData = [];
 		var price;
@@ -177,15 +190,15 @@ exports.getrooms = functions.https.onRequest((request, response) => {
 			snapshot.forEach((doc) => {
 				price = parseInt(doc.data()["price"]);
 				city = doc.data()["city"];
-			
-		
-				console.log('now here2');
-		return admin.firestore().collection('Useen').where("tid","==",recieved).get().then((snapshot) => {		
+
+
+
+		return admin.firestore().collection('Useen').where("uid","==",recieved).get().then((snapshot) => {		
 
 			if (snapshot.empty) {
-				console.log('No matching documents222');
-				
-				
+				console.log('No matching documents111');
+				response.send('No data in database');
+				return;
 			}
 			snapshot.forEach((doc) => {
 				lid = (doc.data()["lid"]);
@@ -193,50 +206,17 @@ exports.getrooms = functions.https.onRequest((request, response) => {
 			
 
 			});
-			
+		
 			console.log('now here');
-			
-				return admin.firestore().collection('Landlordprofile').where("price","<=",price).get().then((snapshot) => {
+				return admin.firestore().collection('Landlordprofile').where("price","==",price).where("city","==",city).get().then((snapshot) => {
 							//.where("lid","not-in",Useenarray)
 					if (snapshot.empty) {
-						console.log('No matching documents333');
+						console.log('No matching documents222');
 						response.send(myData);
 						return;
 					}
 					snapshot.forEach((doc) => {
-						console.log("city"+city);
-						if (doc.data()["city"]==city)
-						{
-						console.log("city match")
-						
-							if (!Useenarray.includes(doc.data()["uid"]))
-							{
-								console.log("not seen")
-								console.log("adding"+doc.data()["uid"]);
-							let docObj = {};
-							docObj.id = doc.id;
-							myData.push(Object.assign(docObj, doc.data()));
-
-							}
-
-						}		
-						
-					});
-					console.log("Sending "+ myData);
-					response.send(myData);
-						console.log("123");
-						//var city = (doc.data()["city"]);
-						//console.log("city"+city);
-						/*return admin.firestore().collection('Landlordprofile').where("city","==",city).get().then((snapshot) => {
-						
-							if (snapshot.empty) {
-								console.log('No matching documents333');
-								response.send(myData);
-								return;
-							}
-							snapshot.forEach((doc) => {
-								console.log("Is match"+doc.data()["uid"])
-							if (!Useenarray.includes(doc.data()["uid"]))
+						if (!Useenarray.includes(doc.data()["uid"]))
 						{
 							console.log("adding"+doc.data()["uid"]);
 							let docObj = {};
@@ -247,30 +227,24 @@ exports.getrooms = functions.https.onRequest((request, response) => {
 						}
 						else
 						console.log("has seen"+doc.data()["uid"]);
+						
+						
 					});
-					console.log("Sending "+ myData);
+
+					console.log(doc.data()["price"]);
+					
+
 					response.send(myData);
-						console.log("123");
-					});
-					
-				});*/
 
-					
-					
-					//console.log("Sending "+ myData);
-					//response.send(myData);
-
-				
+				});
 
 
 			
 
-			//});
+			});
 
 		});
 	});
-});
-});
 });
 });
 
@@ -278,8 +252,6 @@ exports.gettenants = functions.https.onRequest((request, response) => {
 	console.log('here');
 	cors(request, response, () => {
 		var recieved = request.body;
-		//recieved=toString(recieved);
-		console.log("tenant uid"+recieved);
 		let myData = [];
 		var price;
 		var city;
@@ -301,9 +273,9 @@ exports.gettenants = functions.https.onRequest((request, response) => {
 		return admin.firestore().collection('Lseen').where("lid","==",recieved).get().then((snapshot) => {		
 
 			if (snapshot.empty) {
-				console.log('No matching documents222');
+				console.log('No matching documents111');
 				response.send('No data in database');
-				
+				return;
 			}
 			snapshot.forEach((doc) => {
 				uid = (doc.data()["uid"]);
@@ -313,10 +285,10 @@ exports.gettenants = functions.https.onRequest((request, response) => {
 			});
 		
 			console.log('now here');
-				return admin.firestore().collection('userprofile').where("price",">=",price).where("city","==",city).get().then((snapshot) => {
+				return admin.firestore().collection('userprofile').where("price","==",price).where("city","==",city).get().then((snapshot) => {
 							//.where("lid","not-in",Useenarray)
 					if (snapshot.empty) {
-						console.log('No matching documents333');
+						console.log('No matching documents222');
 						response.send(myData);
 						return;
 					}
@@ -358,31 +330,21 @@ exports.like = functions.https.onRequest((request, response) => {
     // 2. Connect to our Firestore database
     cors(request, response, () => {
 
-        
-			console.log("full"+request.body);
-			var recieved = request.body;
-			var tid = request.body.tid;
-			var lid = request.body.lid;
-			console.log("tid1:"+tid);
-			console.log("lid1:"+lid);
-			//var hello = JSON.parse(request.body);
-			//tid =console.log("hiii"+hello.tid);
-			//lid =console.log("hiii"+hello.lid);
-			//tid ="HEY";
-			//lid ="BYE";
-			
+        //const currentTime = admin.firestore.Timestamp.now();
+        //request.body.timestamp = currentTime;
+			let lid = request.body.lid;
+			let tid = request.body.tid;
+			console.log("tid"+request.body.tid);
 			return admin.firestore().collection('likes').where("lid","==",lid).where("tid","==",tid).get().then((snapshot) => {
-				console.log("what is problem");
 				if (snapshot.empty) {
 					console.log('adding like');
-					return admin.firestore().collection('likes').add(recieved).then((snapshot) => {
+					return admin.firestore().collection('likes').add(request.body).then((snapshot) => {
 						console.log("bbb"+request.body);
 						response.send(false);
 				});
 			}
 			else
 			{
-				console.log("now here")
 				return admin.firestore().collection('matches').add(request.body).then((snapshot) => {
 					response.send("true");
 				});
@@ -398,54 +360,3 @@ exports.like = functions.https.onRequest((request, response) => {
     });
 });
 });
-
-exports.useen = functions.https.onRequest((request, response) => {
-    //stores the uid of already viewed profiles
-    cors(request, response, () => {
-
-        
-
-        return admin.firestore().collection('Useen').add(request.body).then((snapshot) => {
-            response.send("Saved in the database");
-        });
-    });
-});
-
-exports.checktype = functions.https.onRequest((request, response) => {
-    //stores the uid of already viewed profiles
-    cors(request, response, () => {
-
-        
-		
-		
-		//var recieved = "ULHWRvGDZ6MWy6mtgnQhJSsC9a42";
-		
-		var recieved = request.body;
-		
-		
-		//var recieved ="ULHWRvGDZ6MWy6mtgnQhJSsC9a42";
-		//notrecieved =notrecieved.replace(/['"]+/g, '');
-		console.log(recieved);
-		//var thisworks="s8I6T9W611dW6YQL0PHt67VjCiu1";
-		
-		//console.log(notrecieved);
-		
-		//console.log("nom"+nom);
-		
-        return admin.firestore().collection('userprofile').where("uid","==",recieved).get().then((snapshot) => {
-
-			if (snapshot.empty) {
-				console.log('No matching documents777');
-				response.send("landlord");
-				return;
-			}
-
-			else 
-			{console.log("sending 1");
-				response.send("tenant");}
-
-        });
-    });
-});
-
-	
