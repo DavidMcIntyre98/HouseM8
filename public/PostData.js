b9541380692fe5c3aa841c5eb12f3eef8e076a34
GooseInTheHoose <57994623+GooseInTheHoose@users.noreply.github.com>
2021-05-06 11:07:55 +0100

    Add files via upload

---
+++ public/PostData.js
@@ -0,0 +1,50 @@
+function PostData()
+{
+    var xhr = new XMLHttpRequest();
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+        xhr.open('POST', 'http://localhost:5001/housem8-8b9bf/us-central1/createprofile');
+    }
+    else {
+        xhr.open('POST', 'https://us-central1-housem8-8b9bf.cloudfunctions.net/createprofile');
+    }
+    xhr.setRequestHeader("Content-type", "application/json");
+    // Track the state changes of the request.
+    xhr.onreadystatechange = function () {
+        var DONE = 4; // readyState 4 means the request is done.
+        var OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                window.location.href= "/match.html";  
+            } else {
+                console.log('Error: ' + xhr.status); // An error occurred during the request.
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(
+        {"uid" : getCookie('uid'),"name": document.getElementById("name").value, "age" : document.getElementById("age").value,"Phone" : document.getElementById("phone").value,"city" : document.getElementById("city").value,"cName" : document.getElementById("cName").value, "PictureURL": document.getElementById('myLink').value, "description": document.getElementById('description').value, "price": parseInt(document.getElementById("price").value)  }
+    ));
+    
+    
+    
+    
+
+
+    //document.getElementById("putCookie").innerHTML=UserId;
+
+
+}
+function getCookie(cname) {
+    var name = cname +"=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
